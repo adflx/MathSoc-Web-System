bc1f38314266b4a3d76d5ab766464745c0ef0393
adflx <ian.felix18@gmail.com>
2018-03-16 18:09:11 +0800

    with amr

---
+++ MathSOC+amr/title.php
@@ -0,0 +1,12 @@
+<!--title -->
+<div class="container-fluid" id="headed">
+	<div class="row">
+	<div class="col-sm-1"></div >
+	<div class="col-sm-10">
+	<a class="mathtitle" href="index.php"> <h1 class="text-center"><img src="image/MATHSOC LOGO.png" height="100px" > TUP Math Society</h1>
+	</a>
+	</div >
+	
+	<div class="col-sm-1"></div >
+	</div>
+	</div>
